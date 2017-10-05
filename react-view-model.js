e773153d7ca3243dbdc0993cae353bc499b682b9
Adam Barrett <adamlbarrett@hotmail.com>
2017-10-05 11:40:24 -0600

    Expose Component and helpers as named imports for ES6 Module syntax

--- react-view-model.js
+++ react-view-model.js
@@ -1,6 +1,9 @@
 var assign = require("can-util/js/assign/assign");
 var Component = require("./component");
 var namespace = require("can-namespace");
+var promise = require("./helpers/promise");
+var autoBindMethods = require("./helpers/autobind-methods");
+var makeEnumerable = require("./helpers/make-enumerable");
 
 module.exports = namespace.reactViewModel = function reactViewModel(displayName, ViewModel, render) {
 	if (arguments.length === 1) {
@@ -55,3 +58,9 @@ module.exports = namespace.reactViewModel = function reactViewModel(displayName,
 
 	return App;
 };
+
+// Expose Component and helpers to named imports
+module.exports.Component = Component;
+module.exports.promise = promise;
+module.exports.autoBindMethods = autoBindMethods;
+module.exports.makeEnumerable = makeEnumerable;

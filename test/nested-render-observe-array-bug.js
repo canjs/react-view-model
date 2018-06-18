36cf5fa558d8492935ff620dfa0d00f62ac92d3a
Christopher Baker <christopher@bitovi.com>
2018-06-18 13:22:01 -0700

    cleanup

--- test/nested-render-observe-array-bug.js
+++ test/nested-render-observe-array-bug.js
@@ -1,50 +1,60 @@
 import QUnit from 'steal-qunit';
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-import { connect } from '../dist/production/ylem';
+import { connect } from 'ylem';
 import { Object as ObserveObject } from 'can-observe';
-
-class MyComponent extends Component {
-	render () {
-		return <div />;
-	}
-}
-
-export class ViewModel extends ObserveObject {
-	constructor () {
-		super();
-		setTimeout(() => {
-			// Setting this synchronously fixes the problem
-			this.items = [{text: 'Hello'}];
-		}, 0);
-	}
-	foo () {}
-  items = []
-}
-
-@connect(ViewModel)
-export default class AppComponent extends Component {
-	render () {
-		const {
-			items,
-			// eslint-disable-next-line no-unused-vars
-			foo
-		} = this.props;
-
-		// Casting this to an Array fixes the problem
-		return items.map((item, key) => (
-			// Changing this to a plain div fixes the problem
-			<MyComponent key={key} />
-		));
-	}
-}
 
 QUnit.module('nested component ObserveArray', () => {
 	QUnit.test('does not render forever', (assert) => {
-		const div = document.createElement('div');
+		const done = assert.async();
 
+		class MyComponent extends Component {
+			render () {
+				return <div>{this.props.text}</div>;
+			}
+		}
+
+		class ViewModel extends ObserveObject {
+			items = []
+
+			constructor () {
+				super();
+
+				setTimeout(() => {
+					// Setting this synchronously fixes the problem
+					this.items = [{ text: 'Hello' }];
+				}, 0);
+			}
+
+			foo () {
+
+			}
+		}
+
+		@connect(ViewModel)
+		class AppComponent extends Component {
+			render () {
+				const {
+					items,
+					foo, // eslint-disable-line no-unused-vars
+				} = this.props;
+
+				// Casting this to an Array fixes the problem
+				return items.map((item, key) => (
+					// Changing this to a plain div fixes the problem
+					<MyComponent key={key} text={item.text} />
+				));
+			}
+		}
+
+		const div = document.createElement('div');
 		ReactDOM.render(<AppComponent />, div);
 
-		assert.equal(div.innerHTML, '<div></div>');
+		assert.equal(div.outerHTML, '<div></div>');
+		setTimeout(() => {
+			assert.equal(div.outerHTML, '<div><div>Hello</div></div>');
+
+			done();
+		}, 100);
 	});
 });
